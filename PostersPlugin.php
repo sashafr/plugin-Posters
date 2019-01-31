578d0715548f33d621ca6c3104e9169dc71875cc
Sasha Renninger <sashafr@upenn.edu>
2019-01-31 17:25:15 +0000

    fixed text on 'add to poster' button to use the plugin option instead of static text

--- PostersPlugin.php
+++ PostersPlugin.php
@@ -23,7 +23,7 @@ define('POSTER_HELP','<h2>Your Posters</h2>'
   * @package Posters
   */
 class PostersPlugin extends Omeka_Plugin_AbstractPlugin
-{   
+{
     // Define Hooks
     protected $_hooks = array(
         'install',
@@ -41,7 +41,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
     protected $_filters = array(
         'admin_navigation_main',
         'guest_user_widgets',
-        'public_navigation_main',  
+        'public_navigation_main',
     );
 
     /**
@@ -51,7 +51,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
     {
         $db = get_db();
         $db->query("CREATE TABLE IF NOT EXISTS {$db->prefix}posters (
-                `id` BIGINT UNSIGNED NOT NULL auto_increment PRIMARY KEY, 
+                `id` BIGINT UNSIGNED NOT NULL auto_increment PRIMARY KEY,
                 `title` VARCHAR(255) NOT NULL,
                 `description` TEXT,
                 `user_id` BIGINT UNSIGNED NOT NULL,
@@ -68,7 +68,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
                 `ordernum` INT NOT NULL
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ;"
         );
-        
+
         set_option('poster_page_path', POSTER_PAGE_PATH);
         set_option('poster_page_title',POSTER_PAGE_TITLE);
         set_option('poster_disclaimer', POSTER_DISCLAIMER);
@@ -76,7 +76,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
         set_option('poster_default_file_type', POSTER_DEFAULT_FILE_TYPE);
         set_option('poster_default_file_type_print', POSTER_DEFAULT_FILE_TYPE_PRINT);
         set_option('poster_show_option', POSTER_SHOW_OPTION);
-    } 
+    }
 
     /**
      * Uninstall this plugin
@@ -85,8 +85,8 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
     {
         $db = get_db();
         $db->query("DROP TABLE IF EXISTS `{$db->prefix}posters`");
-        $db->query("DROP TABLE IF EXISTS `{$db->prefix}poster_items`"); 
-        
+        $db->query("DROP TABLE IF EXISTS `{$db->prefix}poster_items`");
+
         delete_option('poster_page_path');
         delete_option('poster_page_title');
         delete_option('poster_disclaimer');
@@ -113,7 +113,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
         set_option('poster_page_path', preg_replace('/\/+/','',$post['poster_page_path']));
         set_option('poster_page_title',$post['poster_page_title']);
         set_option('poster_disclaimer', $post['poster_disclaimer']);
-        
+
         if (get_option('html_purifier_is_enabled')) {
             $filter = new Omeka_Filter_HtmlPurifier;
             $helpText = $filter->filter($post['poster_help']);
@@ -121,7 +121,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
             $helpText = $post['poster_help'];
         }
         set_option('poster_help', $helpText);
-        
+
         set_option('poster_default_file_type', $post['poster_default_file_type']);
         set_option('poster_default_file_type_print', $post['poster_default_file_type_print']);
         set_option('poster_show_option', $post['poster_show_option']);
@@ -130,7 +130,7 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
     public function hookConfigForm()
     {
         include 'forms/config_form.php';
-       
+
     }
     public function filterAdminNavigationMain($nav)
     {
@@ -141,31 +141,31 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
         );
         return $nav;
     }
-    
+
     public function filterGuestUserWidgets($widgets)
-    { 
+    {
         $bp = get_option('poster_page_path');
         $widget = array('label' => __('Posters'));
         $browse = url("{$bp}/browse");
         $create = url("{$bp}/new");
         $html = "<ul>"
               . "<li><a href='{$browse}'>".__("Browse Posters")."</a></li>"
-              . "<li><a href='{$create}'>".__("New Poster")."</a></li>"            
+              . "<li><a href='{$create}'>".__("New Poster")."</a></li>"
               . "</ul>";
         $widget['content'] = $html;
         $widgets[] = $widget;
         return $widgets;
     }
-    
+
     public function hookDefineAcl($args)
     {
         $acl = $args['acl'];
-        
+
         $acl->addResource('Posters_Poster');
         $acl->allow(null, 'Posters_Poster', array('show','browse'));
         $acl->allow('guest', 'Posters_Poster', array('browse','show','edit', 'add', 'delete'), new Omeka_Acl_Assert_Ownership);
         $acl->allow('guest','Posters_Poster', array('browse','show'));
-        
+
     }
     public function filterPublicNavigationMain($nav)
     {
@@ -221,20 +221,21 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
 
     }
 
-    /** 
+    /**
      * Added to place an add item to poster button on each
      * item in the browse page and on item show pages
-     * 
+     *
      * Sasha Renninger & Josh Berg, Penn DS Team, 2017
      */
     public function hookPublicItemsBrowseEach($args) {
         if(current_user()){
             $user = current_user();
             $user_posters = get_records('Poster',array('user_id'=>$user->id));
-            
+            $pageTitle = html_escape(get_option('poster_page_title'));
+
             echo '<div class="dropdown">
                     <button type="button" class="btn btn-default dropdown-toggle" id="dropdown-' . $args['item']->id. '" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Add to Poster
+                        Add to ' . $pageTitle . '
                     </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdown-' . $args['item']->id. '">';
             foreach($user_posters as $poster) {
@@ -244,12 +245,12 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
 
         }
     }
-    
+
     public function hookPublicItemsShow($args) {
         if(current_user()){
             $user = current_user();
             $user_posters = get_records('Poster',array('user_id'=>$user->id));
-            
+
             echo '<div class="dropdown">
                     <button type="button" class="btn btn-default dropdown-toggle" id="dropdown-' . $args['item']->id. '" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Add to Poster
@@ -261,8 +262,8 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
             echo '</ul></div>';
 
         }
-    }    
-    
+    }
+
     /**
      * Needed for dropdown in previous hook to work
      */
@@ -270,8 +271,8 @@ class PostersPlugin extends Omeka_Plugin_AbstractPlugin
         echo '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>';
         echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>';
         echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>';
-        
+
         echo '<style>.dropdown-menu{display: none; z-index: 1000; background-color: #fff; background-clip: padding-box; border: 1px solid rgba(0,0,0,.15); border-radius: .25rem; float: left; min-width: 10rem; padding: .5rem 0; margin: .125rem 0 0; text-align: left;}.dropdown-menu.show{display: block;}.dropdown-item{display: block; width:100%; clear: both; background-color: transparent; border: 0; padding: .25rem 1.5rem; margin: .125rem 0 0;}</style>';
-    }    
+    }
 
 }
